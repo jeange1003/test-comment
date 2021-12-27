ebad3c37b3b2cb7d14c8283d277a1fb940be9d7b
JeanGe <jeange1003@163.com>
2021-12-27 18:52:33 +0800

    aaaa

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+import {a} from './a.js';
+
+
 console.log('hello world')
 
-const a  = 'hello world'
 console.log(a)
+// const a  = 'hello world'
+// console.log(a)
 
 const b = '提高效率，早点下班'
 console.log(b)
@@ -11,3 +15,4 @@ console.log('c')
 
 const d = 'greedisgood 1000000'
 console.log(d)
+
